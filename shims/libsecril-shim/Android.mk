66e8b2eace24624ad1d2f290bd9f9dfe769bb192
Shilin Victor <chrono.monochrome@gmail.com>
2018-10-27 16:05:18 +0300

    shims: move to common [1/2]
    Change-Id: I92067269fa73bfe412a422ab123c5529044020c8

--- shims/libsecril-shim/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH:= $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES:= secril-shim.c
-LOCAL_SHARED_LIBRARIES := liblog libbinder
-LOCAL_MODULE:= libsecril-shim
-
-include $(BUILD_SHARED_LIBRARY)
