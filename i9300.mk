0eadeb8e14d078fc634d829c0b83121c5fcf50d4
Shilin Victor <chrono.monochrome@gmail.com>
2019-02-14 19:01:37 +0300

    Revert "i9300: add RIL subscripttion workaround"
    This reverts commit 989ff3e193d1ce13cce1f1bdf169eee5a1a9b2c9.
    
    Change-Id: Ie1bd8b9fac3b15a974f9dafbd0518acb30f0afd8

--- i9300.mk
+++ i9300.mk
@@ -51,11 +51,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.xml:system/etc/gps.xml \
     $(LOCAL_PATH)/gps_daemon.sh:system/bin/gps_daemon.sh
 
-# RIL subscription workaround
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/restart_rild.sh:system/bin/restart_rild.sh \
-    $(LOCAL_PATH)/configs/rild_restart.rc:system/etc/init/rild_restart.rc
-
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl
