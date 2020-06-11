4388df60209631bb8fd862def6d756e014147c37
Dominggoes Isakh <drjisakh@gmail.com>
2020-06-11 11:05:37 +0200

    i9300: libgps-shim: Update for GNSS HIDL
    Change-Id: I3deabbf25b7c488b0fe308c18142b31e63d7d9cf

--- shims/libgps-shim/gps.h
+++ shims/libgps-shim/gps.h
@@ -15,6 +15,56 @@
  */
 #include <hardware/gps.h>
 
+/**
+ * Legacy struct to represents SV information.
+ * Deprecated, to be removed in the next Android release.
+ * Use GnssSvInfo instead.
+ */
+typedef struct {
+    /** set to sizeof(GpsSvInfo) */
+    size_t          size;
+    /** Pseudo-random number for the SV. */
+    int     prn;
+    /** Signal to noise ratio. */
+    float   snr;
+    /** Elevation of SV in degrees. */
+    float   elevation;
+    /** Azimuth of SV in degrees. */
+    float   azimuth;
+    /** Unknown field in Samsung I9100 libgps
+        May be an indicator for constellation type
+        (GPS, GLONASS, Galileo)?
+        Used on GT-I9100, likely also present on GT-N7000,
+        SGH-I717, SGH-I727 but this needs confirmation.
+    */
+    int vendor;
+} GpsSvInfo_vendor;
+
+typedef struct {
+    /** set to sizeof(GpsSvStatus) */
+    size_t size;
+    int num_svs;
+    GpsSvInfo_vendor sv_list[GPS_MAX_SVS];
+    uint32_t ephemeris_mask;
+    uint32_t almanac_mask;
+    uint32_t used_in_fix_mask;
+} GpsSvStatus_vendor;
+
+
+typedef struct {
+    /** set to sizeof(GpsCallbacks_vendor) */
+    size_t size;
+    gps_location_callback location_cb;
+    gps_status_callback status_cb;
+    gps_sv_status_callback sv_status_cb;
+    gps_nmea_callback nmea_cb;
+    gps_set_capabilities set_capabilities_cb;
+    gps_acquire_wakelock acquire_wakelock_cb;
+    gps_release_wakelock release_wakelock_cb;
+    gps_create_thread create_thread_cb;
+    gps_request_utc_time request_utc_time_cb;
+} GpsCallbacks_vendor;
+
 /* CellID for 2G, 3G and LTE, used in AGPS. */
 typedef struct {
     AGpsRefLocationType type;
@@ -27,23 +77,24 @@ typedef struct {
      * might rely in the old (wrong) behavior.
      */
     uint16_t lac;
+    uint16_t psc;
     /** Cell id in 2G. Utran Cell id in 3G. Cell Global Id EUTRA in LTE. */
     uint32_t cid;
-#if 0 // introduced in N.
-    /** Tracking Area Code in LTE. */
-    uint16_t tac;
-    /** Physical Cell id in LTE (not used in 2G and 3G) */
-    uint16_t pcid;
-#endif
-} AGpsRefLocationCellIDNoLTE;
+} AGpsRefLocationCellID_vendor;
 
 /** Represents ref locations */
 typedef struct {
     AGpsRefLocationType type;
     union {
-        AGpsRefLocationCellIDNoLTE	cellID;
-		AGpsRefLocationMac			mac;
+        AGpsRefLocationCellID_vendor	cellID;
+	AGpsRefLocationMac		mac;
     } u;
-} AGpsRefLocationNoLTE;
+} AGpsRefLocation_vendor;
 
-
+typedef struct {
+    float accuracy;
+    bool enabled;
+    double locdiff;
+    float speed;
+    GpsUtcTime timeout;
+} GpsFilterLocation;
