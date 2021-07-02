b396d8de02085c38fbef8c8cd066a3b55b247366
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-07-02 18:36:39 +0200

    Move audio config file to vendor.

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -2962,7 +2962,7 @@ static int adev_config_parse(struct m0_audio_device *adev)
     int len;
 
     property_get("ro.product.device", property, "tiny_hw");
-    snprintf(file, sizeof(file), "/system/etc/sound/%s", property);
+    snprintf(file, sizeof(file), "/vendor/etc/sound/%s", property);
 
     ALOGV("Reading configuration from %s\n", file);
     f = fopen(file, "r");
