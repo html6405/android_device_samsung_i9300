caded40f92597a35262bc9ff2695f67bc5266bee
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-07-05 15:40:38 +0200

    Fix inherit logic.

--- lineage_i9300.mk
+++ lineage_i9300.mk
@@ -2,8 +2,8 @@
 PRODUCT_RELEASE_NAME := i9300
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/smdk4412-common/common.mk)
 $(call inherit-product, device/samsung/i9300/i9300.mk)
+$(call inherit-product, device/samsung/smdk4412-common/common.mk)
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
