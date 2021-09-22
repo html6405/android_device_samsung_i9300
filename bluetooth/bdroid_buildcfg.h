e62d999079567bb464d2cdb97fe59f55b2514ef6
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-09-22 17:08:46 +0200

    bluetooth: Update bluetooth config

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -20,6 +20,15 @@
 
 #define BTM_DEF_LOCAL_NAME   "GT-I9300"
 
+#define BTA_DISABLE_DELAY 100 /* in milliseconds */
+#define SC_MODE_INCLUDED FALSE
+#define SDP_DEBUG FALSE
+#define BTSNOOP_MEM FALSE
+#define BLE_PRIVACY_SPT FALSE
+#define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
+#define BTM_WBS_INCLUDED TRUE /* Enable WBS */
+#define BTIF_HF_WBS_PREFERRED FALSE /* Do not prefer WBS */
+
 /* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
 #define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
 
